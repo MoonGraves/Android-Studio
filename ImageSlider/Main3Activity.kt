7af0b903bd91b065ce3249659f1c5aa39895e4de
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-01-13 21:01:07 +0200

    Add files via upload

---
+++ ImageSlider/Main3Activity.kt
@@ -0,0 +1,42 @@
+package com.example.location2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main3.*
+
+class Main3Activity : AppCompatActivity() {
+
+    //Read class file Adapter
+    lateinit var adapters: Adapters
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        setSupportActionBar(toolbar)
+
+
+        //TODO: add lista mitä kuvia haluat näyttää siinä sovelluksessa, kaiken lisäksi kuvan nimi pitää olla pienellä..? normaalisti pitäisi hyväksyä iso/pien kirjaimia
+        var list = mutableListOf<Int>()
+
+        /*list.add(R.drawable.one)
+        list.add(R.drawable.two)
+        list.add(R.drawable.three)
+        list.add(R.drawable.four)
+         */
+        list.add(R.drawable.sieppaaakkari)
+        list.add(R.drawable.five)
+        list.add(R.drawable.one)
+        list.add(R.drawable.six)
+        list.add(R.drawable.seven)
+        list.add(R.drawable.eight)
+
+
+        adapters = Adapters(this)
+        adapters.setContentList(list)
+        viewpager.adapter = adapters
+
+    }
+
+}
